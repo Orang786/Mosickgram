348041736a8943a66135a645e949c5bb1a7017c1
xX_PussyDestroyed_Xx <147304905+Orang786@users.noreply.github.com>
2026-02-01 14:58:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -326,3 +326,4 @@ async function handleCommand(socket, user, text) {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log('Server running on port ' + PORT));
+
